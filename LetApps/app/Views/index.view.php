9b4148b41a0af2a425337c010ac8b96a3921a01c
Equipo de LetPHP <team@letphp.run>
2020-07-05 18:02:34 -0500

    Agregamos HTML para la presentación.

--- LetApps/app/Views/index.view.php
+++ LetApps/app/Views/index.view.php
@@ -1,1 +1,18 @@
-<h1>Bienvenido a {site_title}</h1>
+<div class="let--container">
+	<div class="let--logo">
+		<img src="./letphp.ico"/>
+	</div>
+	<div class="let--welcome">
+		<h1>Gracias por ser parte de <span class="let--site-name" >{site_name}</span></h1>
+	</div>
+	
+	<div class="let--menu">
+		<ul>
+			{each values=$aItems value=aItem}			
+				<li class="let--menu-item" >
+					<a href="{$aItem.route}" target="_blank" >{$aItem.text}</a>
+				</li>
+			{/each}
+		</ul>
+	</div>
+</div>
