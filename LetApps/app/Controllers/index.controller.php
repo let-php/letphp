88900197c648168123f4bdcc7456ae4c172bc878
Equipo de LetPHP <team@letphp.run>
2020-07-05 17:58:30 -0500

    Hemos complementado el controlador Index

--- LetApps/app/Controllers/index.controller.php
+++ LetApps/app/Controllers/index.controller.php
@@ -6,7 +6,16 @@ class Index_Controller
 {
 	public function start()
 	{
+		$oView = View();
 		
+		$aItems = [
+			['key' => 'letphp', 'route' => 'https://letphp.run' , 'text' => 'LetPHP'],
+			['key' => 'docs', 'route' => 'https://docs.letphp.run' , 'text' => 'Documentación'],
+			['key' => 'videos', 'route' => 'https://videos.letphp.run' , 'text' => 'Videos'],
+			['key' => 'blog', 'route' => 'https://blog.letphp.run' , 'text' => 'Blog'],
+			['key' => 'expo', 'route' =>  'https://expo.letphp.run', 'text' => 'Proyectos'],
+		];
+		$oView->setCss(['letphp.css' => 'app_app'])->setValues(['aItems' => $aItems]);
 	}
 }
     
